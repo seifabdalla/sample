e28dcde80071a7a60506d1531dc2366d3711bd45
Seif Abdallah <seif1281@gmail.com>
2023-07-18 15:13:36 +0300

    initial

---
+++ config.js
@@ -0,0 +1,90 @@
+var config = {}
+
+config.endpoint = 'https://f197779a-0ee0-4-231-b9ee.documents.azure.com:443/;AccountKey=N3YyvZDLtX49ZzL0Ju0RKyzYbmhpMFLiHQaeuvGNbppBOV0Bn9PKgcUiO8qoPzq4l8kfCpKhSseNACDbUiZ14g=='
+config.key = 'N3YyvZDLtX49ZzL0Ju0RKyzYbmhpMFLiHQaeuvGNbppBOV0Bn9PKgcUiO8qoPzq4l8kfCpKhSseNACDbUiZ14g=='
+config.database = {
+  id: 'db'
+}
+
+config.container = {
+  id: 'items'
+}
+
+config.items = {
+
+
+  Andersen: {
+    id: 'Anderson.1',
+    Country: 'USA',
+    lastName: 'Andersen',
+    parents: [
+      {
+        firstName: 'Thomas'
+      },
+      {
+        firstName: 'Mary Kay'
+      }
+    ],
+    children: [
+      {
+        firstName: 'Henriette Thaulow',
+        gender: 'female',
+        grade: 5,
+        pets: [
+          {
+            givenName: 'Fluffy'
+          }
+        ]
+      }
+    ],
+    address: {
+      state: 'WA',
+      county: 'King',
+      city: 'Seattle'
+    }
+  },
+  Wakefield: {
+    id: 'Wakefield.7',
+    Country: 'Italy',
+    parents: [
+      {
+        familyName: 'Wakefield',
+        firstName: 'Robin'
+      },
+      {
+        familyName: 'Miller',
+        firstName: 'Ben'
+      }
+    ],
+    children: [
+      {
+        familyName: 'Merriam',
+        firstName: 'Jesse',
+        gender: 'female',
+        grade: 8,
+        pets: [
+          {
+            givenName: 'Goofy'
+          },
+          {
+            givenName: 'Shadow'
+          }
+        ]
+      },
+      {
+        familyName: 'Miller',
+        firstName: 'Lisa',
+        gender: 'female',
+        grade: 1
+      }
+    ],
+    address: {
+      state: 'NY',
+      county: 'Manhattan',
+      city: 'NY'
+    },
+    isRegistered: false
+  }
+}
+
+module.exports = config
