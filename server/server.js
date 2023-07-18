7b6b373e3580af3663b0554de6023a18aa6a20ab
Seif Abdallah <seif1281@gmail.com>
2023-07-18 22:16:05 +0300

    game initial

--- server/server.js
+++ server/server.js
@@ -4,10 +4,36 @@ const express = require('express');
 // Create an express app object
 const app = express();
 
+let games=[]
 // Define a GET handler for the / route
-app.get('/', (req, res) => {
+app.get('/games', (req, res) => {
   // Send the sample text as the response
-  res.send('Hello, this is a simple server in node.js using express');
+  res.status(200).json(games);
+});
+const { v4: uuidv4 } = require('uuid');
+
+app.post('/games', (req,res)=> {
+  let uuid = uuidv4();
+  const game= {
+    id:uuid
+  };
+  games.push(game);
+  res.status(201).json(games);
+  
+  
+})
+
+// Define a DELETE handler for the /games/:id route
+app.delete("/games/:id", (req, res) => {
+  const id  = req.params.id;
+  const index = games.findIndex((g) => g.id === id);
+  if (index !== -1) {
+    const deletedGame = games.splice(index, 1);
+    res.status(200).json(games);
+  }
+  else {
+    res.status(404).json({ message: 'Game not found' });
+  }
 });
 
 // Listen on port 3000
