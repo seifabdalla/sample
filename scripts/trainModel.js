2cf98ffa7bb688fd86e9012fb32c4b36ac1e081f
Mahmoud Mamdouh <mahmoud1905286@miuegypt.edu.eg>
2023-08-10 16:15:43 +0300

    3

--- scripts/trainModel.js
+++ scripts/trainModel.js
@@ -1,90 +1,65 @@
-const LogisticRegression = require('logistic-regression');
+const { CosmosClient } = require('@azure/cosmos');
 const fs = require('fs');
-const math = require('mathjs');
-const { loadAndPreprocessData } = require('./dataRetrieval'); // Adjust the path as needed
+const path = require('path');
 
-// Utility function for train-test split
-function trainTestSplit(data, labels, testRatio) {
-  const splitIndex = Math.floor(data.length * testRatio);
-  const xTrain = data.slice(0, splitIndex);
-  const xTest = data.slice(splitIndex);
-  const yTrain = labels.slice(0, splitIndex);
-  const yTest = labels.slice(splitIndex);
-  return { xTrain, xTest, yTrain, yTest };
+// Configure connection to Cosmos DB
+const endpoint = 'YOUR_COSMOS_DB_URI';
+const key = 'YOUR_COSMOS_DB_ACCESS_KEY';
+
+const client = new CosmosClient({ endpoint, key });
+
+// Preprocessing logic for the specific company
+function preprocessCompanyData(companyData) {
+  // Preprocess data for the specific company
+  const trainingData = companyData.map(item => {
+    // Implement preprocessing logic to extract features from the item
+    // and create the input data for the model
+    // For simplicity, let's assume the input features are 'open', 'high', 'low', and 'close'
+    return [item.open, item.high, item.low, item.close];
+  });
+
+  const trainingLabels = companyData.map(item => {
+    // Implement logic to create labels (e.g., 0 for "hold", 1 for "buy", 2 for "sell")
+    // based on the item's properties
+    // For simplicity, let's assume the label is encoded as follows:
+    // 'hold': 0, 'buy': 1, 'sell': 2
+    if (item.decision === 'hold') return 0;
+    else if (item.decision === 'buy') return 1;
+    else if (item.decision === 'sell') return 2;
+    else return -1; // Undefined label, handle this as needed
+  });
+
+  return { trainingData, trainingLabels };
 }
 
-// Utility functions for evaluation metrics
-function accuracyScore(trueLabels, predictedLabels) {
-  let correctCount = 0;
-  for (let i = 0; i < trueLabels.length; i++) {
-    if (trueLabels[i] === predictedLabels[i]) {
-      correctCount++;
-    }
-  }
-  return correctCount / trueLabels.length;
+// Load and preprocess data from Cosmos DB for each company
+async function loadAndPreprocessDataForCompany(companySymbol) {
+  // Cosmos DB configuration
+  const databaseId = 'YourDatabaseId';
+  const containerId = 'YourContainerId';
+
+  const database = client.database(databaseId);
+  const container = database.container(containerId);
+
+  // Query or fetch documents from the container for the specific company
+  const query = 'SELECT * FROM c WHERE c.Stock_Symbol = @symbol';
+  const parameters = [
+    { name: '@symbol', value: companySymbol }
+  ];
+
+  const { resources: companyData } = await container.items.query(query, parameters).fetchAll();
+
+  // Preprocess data for the specific company
+  const { trainingData, trainingLabels } = preprocessCompanyData(companyData);
+
+  // Save the preprocessed data to a JSON file
+  const companyDataFilePath = path.join(__dirname, `${companySymbol}.json`);
+  fs.writeFileSync(companyDataFilePath, JSON.stringify({ trainingData, trainingLabels }, null, 2));
+
+  console.log(`Data for company ${companySymbol} saved to ${companyDataFilePath}`);
 }
 
-function precisionScore(trueLabels, predictedLabels) {
-  let truePositives = 0;
-  let falsePositives = 0;
+// Example usage: Load and preprocess data for a specific company (e.g., 'AAPL')
+loadAndPreprocessDataForCompany('AAPL');
 
-  for (let i = 0; i < trueLabels.length; i++) {
-    if (trueLabels[i] === 1 && predictedLabels[i] === 1) {
-      truePositives++;
-    } else if (trueLabels[i] === 0 && predictedLabels[i] === 1) {
-      falsePositives++;
-    }
-  }
-
-  return truePositives / (truePositives + falsePositives);
-}
-
-function recallScore(trueLabels, predictedLabels) {
-  let truePositives = 0;
-  let falseNegatives = 0;
-
-  for (let i = 0; i < trueLabels.length; i++) {
-    if (trueLabels[i] === 1 && predictedLabels[i] === 1) {
-      truePositives++;
-    } else if (trueLabels[i] === 1 && predictedLabels[i] === 0) {
-      falseNegatives++;
-    }
-  }
-
-  return truePositives / (truePositives + falseNegatives);
-}
-
-function f1Score(trueLabels, predictedLabels) {
-  const precision = precisionScore(trueLabels, predictedLabels);
-  const recall = recallScore(trueLabels, predictedLabels);
-
-  return (2 * precision * recall) / (precision + recall);
-}
-async function trainModel() {
-  const { trainingData, trainingLabels } = await loadAndPreprocessData();
-
-  // Split data into training and testing sets
-  const { xTrain, xTest, yTrain, yTest } = trainTestSplit(trainingData, trainingLabels, 0.8);
-
-  // Create and train the model
-  const model = new LogisticRegression();
-  model.fit(xTrain, yTrain);
-
-  // Validate the model
-  const yPred = model.predict(xTest);
-  const accuracy = accuracyScore(yTest, yPred);
-  const precision = precisionScore(yTest, yPred);
-  const recall = recallScore(yTest, yPred);
-  const f1 = f1Score(yTest, yPred);
-
-  console.log(`Accuracy: ${accuracy}`);
-  console.log(`Precision: ${precision}`);
-  console.log(`Recall: ${recall}`);
-  console.log(`F1-Score: ${f1}`);
-}
-
-// Run the training and evaluation process
-trainModel().catch(error => {
-  console.error('An error occurred:', error);
-});
-
+module.exports = { loadAndPreprocessDataForCompany };
